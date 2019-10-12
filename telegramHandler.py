68bb1e45fbd5056d6bd6ff21e793c773105dcd2e
xuankai <xkgoh1@gmail.com>
2019-10-12 16:32:05 +0800

    Created new function to execute lambda function. Created method to paginate. Changed answerCallBack function to only execute after page is reloaded.

--- telegramHandler.py
+++ telegramHandler.py
@@ -5,12 +5,14 @@ import os
 import sys
 import math
 from geopy.distance import geodesic
+import telegramHandlerDBWriter
 
-MAX_NUM_RESULTS_RETURNED = 30
+MAX_NUM_RESULTS_PER_PAGE = 20
 RADIUS_CHANGE_FACTOR = 0.65  # 85% change
 APPROVED_CATEGORIES = [1]
 APPROVED_USER_STR = os.environ['APPROVED_USER_LIST']
 TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
+BASE_URL = "https://api.telegram.org/bot{}".format(TOKEN)
 
 
 # Compute the distance between two pairs of coordinates
@@ -20,28 +22,61 @@ def compute_distance(latitude_A, longitude_A, latitude_B, longitude_B):
     return geodesic(location_A, location_B).meters
 
 
+def paginate_results(json_response, page_number):
+    print "Paginating results."
+    print json_response
+    print type(json_response)
+    json_result = {'searchRadius': json_response['searchRadius'],
+                    'searchCenterLatitude': json_response['searchCenterLatitude'],
+                    'searchCenterLongitude': json_response['searchCenterLongitude']}
+    json_location_temp_arr = []
+
+    print "check 1"
+
+    start_index = min((page_number-1)*MAX_NUM_RESULTS_PER_PAGE, len(json_response['locations']))  # Index starts from 0
+    end_index = min(page_number*MAX_NUM_RESULTS_PER_PAGE, len(json_response['locations']))  # Index ends at length-1
+
+    print "check 2"
+
+    if (start_index == end_index and start_index == len(json_response['locations'])):
+        print "No contents for this page"
+        return
+
+    for i in range(start_index, end_index):
+        json_location_temp_arr.append(json_response['locations'][i])
+
+    print "check 3"
+
+    json_result['locations'] = json_location_temp_arr
+    json_result['startItemNumber'] = start_index + 1
+    json_result['endItemNumber'] = end_index
+    json_result['totalItems'] = len(json_response['locations'])
+    print "Paginating completed."
+    return json_result
+
 # Filter merchant details based on the top_N closest merchant from the center
-def filter_merchant_top_N(json_response, center_lat_lng, top_N):
-    print "EXECUTING TOP N"
-    json_cleaned = {'searchRadius': json_response['searchRadius']}
+def sort_results_by_distance(json_response, center_lat_lng):
+    print "Executing sorting by distance"
+    json_cleaned = {'searchRadius': json_response['searchRadius'],
+                    'searchCenterLatitude': center_lat_lng['latitude'],
+                    'searchCenterLongitude': center_lat_lng['longitude']}
     json_location_cleaned_arr = []
     location_temp_arr = []
     center_latitude, center_longitude = center_lat_lng['latitude'], center_lat_lng['longitude']
 
-    # Compute the distance for each merchant
+    # Compute the distance for each merchant and update the merchant json information with the distance
     for merchant in json_response['locations']:
         coordinates = json.loads(str(merchant['geoJson']['S']))
         latitude, longitude = coordinates['coordinates'][1], coordinates['coordinates'][0]
         distance = compute_distance(latitude, longitude, center_latitude, center_longitude)
+        merchant['distanceFromCenter'] = distance
         location_temp_arr.append((distance, merchant))
-    location_temp_arr.sort(key=lambda tup: tup[0])
+    location_temp_arr.sort(key=lambda tup: tup[0])  # Sort the merchants by distance
     print location_temp_arr
 
     # Add the sorted merchant list back into the array
-    for i in range(min(MAX_NUM_RESULTS_RETURNED, len(location_temp_arr))):
+    for i in range(len(location_temp_arr)):
         json_location_cleaned_arr.append(location_temp_arr[i][1])
-        if i == min(MAX_NUM_RESULTS_RETURNED, len(location_temp_arr))-1:
-            json_cleaned['searchRadius'] = location_temp_arr[i][0]
 
     json_cleaned['locations'] = json_location_cleaned_arr
     print json_cleaned
@@ -63,7 +98,8 @@ def filter_merchant_category(json_response):
 
 
 # Convert a json_reply from dynamodb into a telegram markdown reply
-def format_json_response(json_response, center, radius, original_merchant_length):
+# def format_json_response(json_response, center, radius, original_merchant_length):
+def format_json_response(json_response):
     if json_response is None:
         return "Sorry there are no results :("
     # locations = json.loads(json_response)
@@ -74,8 +110,8 @@ def format_json_response(json_response, center, radius, original_merchant_length
 
     # Format the onemap_url with the centerpoint
     onemap_url = "https://developers.onemap.sg/commonapi/staticmap/getStaticImage?layerchosen=default&lat=" \
-                 + str(center['latitude']) + "&lng=" + str(center['longitude']) + "&zoom=15&height=512&width=512&points=[" \
-                 + str(center['latitude']) + "," + str(center['longitude']) + ",%22255,0,0%22]"
+                 + str(json_response['searchCenterLatitude']) + "&lng=" + str(json_response['searchCenterLongitude']) + "&zoom=15&height=512&width=512&points=[" \
+                 + str(json_response['searchCenterLatitude']) + "," + str(json_response['searchCenterLongitude']) + ",%22255,0,0%22]"
 
     output_string = ""
     counter = 'A'
@@ -98,9 +134,9 @@ def format_json_response(json_response, center, radius, original_merchant_length
 
         # Print the data source
         if data_source == 1:
-            output_string = output_string + "[(The Entertainer)](" + str(additional_details['SourceWebsite']) + ")"
+            output_string = output_string + "[(Entnr)](" + str(additional_details['SourceWebsite']) + ")"
         elif data_source == 2:
-            output_string = output_string + "[(Citibank)](" + str(additional_details['SourceWebsite']) + ")"
+            output_string = output_string + "[(Citi)](" + str(additional_details['SourceWebsite']) + ")"
 
         # Print additional info about discount/deal
         if additional_details.get('OfferDetails') and len(str(additional_details['OfferDetails'])) < 100:
@@ -113,12 +149,33 @@ def format_json_response(json_response, center, radius, original_merchant_length
         counter = chr(ord(counter)+1) # Increment to the next alphabet
 
     # Add in the onemap_url
-    if original_merchant_length != len(locations['locations']):
-        output_string = "*Cheapo found*[ ](" + onemap_url + ")*" + str(original_merchant_length) + " results in a 1000m radius!*\nShowing top " + str(len(locations['locations'])) + " results within " + str(radius) + "m...\n\n" + output_string
-    else:
-        output_string = "*Cheapo found*[ ](" + onemap_url + ")*" + str(len(locations['locations'])) + " results in a " + str(radius) + "m radius!*\n\n" + output_string
+    # if original_merchant_length != len(locations['locations']):
+    output_string = "*Cheapo found*[ ](" + onemap_url + ")*" + str(json_response['totalItems']) + " results in a " + str(json_response['searchRadius']) + "m radius!*\nDisplaying results " + str(json_response['startItemNumber']) + " to " + str(json_response['endItemNumber']) + "\n\n" + output_string
+    # else:
+    #     output_string = "*Cheapo found*[ ](" + onemap_url + ")*" + str(len(locations['locations'])) + " results in a " + str(radius) + "m radius!*\n\n" + output_string
     # print output_string
     return output_string
+
+
+# Invoke a lambda function and returns the json data returned
+def invoke_lambda_function(function_name, invocation_type, payload_string):
+    lambda_client = boto3.client('lambda')
+    invoke_response = lambda_client.invoke(FunctionName=function_name,
+                                           InvocationType=invocation_type,
+                                           Payload=payload_string)
+    return json.loads(invoke_response['Payload'].read())
+
+
+# Create the reply markup keyboard based on the page number
+def create_reply_markup(current_page, max_page):
+    reply_markup = {}
+    if 1 < current_page < max_page:
+        reply_markup = {"inline_keyboard": [[{"text": "Page " + str(current_page - 1), "callback_data": current_page - 1}, {"text": "Page " + str(current_page + 1), "callback_data": current_page + 1}]]}
+    elif current_page == 1:
+        reply_markup = {"inline_keyboard": [[{"text": "Page " + str(current_page + 1), "callback_data": current_page + 1}]]}
+    elif current_page == max_page:
+        reply_markup = {"inline_keyboard": [[{"text": "Page " + str(current_page - 1), "callback_data": current_page - 1}]]}
+    return reply_markup
 
 
 def lambda_handler(event, context):
@@ -128,20 +185,34 @@ def lambda_handler(event, context):
         print event
 
         if "callback_query" in data:
+            reply_url = BASE_URL + "/editMessageText"
+            source_message_id = data["callback_query"]["message"]["message_id"]
+            source_chat_id = data["callback_query"]["message"]["chat"]["id"]
+            current_page = int(data["callback_query"]["data"])
+            reply_item = telegramHandlerDBWriter.get_from_result_cache(source_chat_id)
+            print "reply is"
+            print reply_item
+            json_response = paginate_results(json.loads(reply_item['Result']), current_page)
+            # markdown_reply = format_json_response(json_response, message["location"], search_parameters['radius'],
+            #                                       original_merchant_length)
+            print "paginate "
+            markdown_reply = format_json_response(json_response)
+            print "mark down reply is"
+            print markdown_reply
+
+            reply_markup = create_reply_markup(current_page, int(math.ceil(float(json_response['totalItems'])/float(MAX_NUM_RESULTS_PER_PAGE))))
+
+            # reply_markup = {"inline_keyboard":[[{"text": "Page "+str(current_page+1), "callback_data": current_page+1}]]}
+            reply_data = {"chat_id": source_chat_id, "message_id": source_message_id, "text": markdown_reply.encode("utf8"), "parse_mode": "markdown", "reply_markup": json.dumps(reply_markup)}
+            post_reply = requests.post(reply_url, reply_data)
+            print str(post_reply.text)
             print "executing call back query"
-            BASE_URL = "https://api.telegram.org/bot{}".format(TOKEN)
             reply_url = BASE_URL + "/answerCallbackQuery"
             callback_query_id = data["callback_query"]["id"]
             reply_data = {"callback_query_id": callback_query_id}
             requests.post(reply_url, reply_data)
             print "executed callback query"
-            reply_url = BASE_URL + "/editMessageText"
-            source_message_id = data["callback_query"]["message"]["message_id"]
-            source_chat_id = data["callback_query"]["message"]["chat"]["id"]
-            reply_data = {"chat_id": source_chat_id, "message_id": source_message_id, "text": "GOOD GOOD"}
-            post_reply = requests.post(reply_url, reply_data)
-            print str(post_reply.text)
-            return
+            return {"statusCode": 200}
 
         # If it is replying to a message not sent by the chatbot, ignore it
         if "reply_to_message" in data["message"]:
@@ -159,7 +230,7 @@ def lambda_handler(event, context):
         APPROVED_USER_LIST = map(long, APPROVED_USER_STR.split(", "))
 
         # Get telegram token handler
-        BASE_URL = "https://api.telegram.org/bot{}".format(TOKEN)
+        # BASE_URL = "https://api.telegram.org/bot{}".format(TOKEN)
         reply_url = BASE_URL + "/sendMessage"
 
         # Check the telegram ID to see if user if approved
@@ -179,29 +250,25 @@ def lambda_handler(event, context):
                 data = {"text": ("Hello I am Cheapo! Reply this message to talk to me!").encode("utf8"), "chat_id": chat_id, "reply_to_message_id": source_message_id}
                 requests.post(reply_url, data)
             else:
-                lambda_client = boto3.client('lambda')
-                invoke_response = lambda_client.invoke(FunctionName="blankLambda",
-                                                       InvocationType='RequestResponse',
-                                                       Payload=json.dumps(message["text"])
-                                                       )
-                data1 = invoke_response['Payload'].read()
+                data1 = invoke_lambda_function("blankLambda", "RequestResponse", json.dumps(message["text"]))
+
                 data = {"text": (str(data1)+" Text").encode("utf8"), "chat_id": chat_id, "reply_to_message_id": source_message_id}
                 requests.post(reply_url, data)
                 print "Lambda client invoked"
                 print data1
 
         if "location" in message:
-            print "Coordinates received."
-            lambda_client = boto3.client('lambda')
-            invoke_response = lambda_client.invoke(FunctionName="queryGeoDatabase",
-                                                   InvocationType='RequestResponse',
-                                                   Payload=json.dumps(message["location"]))
+            json_response = invoke_lambda_function("queryGeoDatabase", "RequestResponse", json.dumps(message["location"]))
             print "Lambda client invoked"
-            json_response = json.loads(invoke_response['Payload'].read())
             json_response = filter_merchant_category(json_response)
             original_merchant_length = len(json_response['locations'])
-            if len(json_response['locations']) > MAX_NUM_RESULTS_RETURNED:
-                json_response = filter_merchant_top_N(json_response, message["location"], MAX_NUM_RESULTS_RETURNED)
+            json_response = sort_results_by_distance(json_response, message["location"])
+            telegramHandlerDBWriter.write_to_results_cache(chat_id, json_response)
+            json_response = paginate_results(json_response, 1)
+
+            if len(json_response['locations']) > MAX_NUM_RESULTS_PER_PAGE:
+                print ""
+                # paginate_results
 
 
 
@@ -229,12 +296,13 @@ def lambda_handler(event, context):
 
 
             # json_response = invoke_response['Payload'].read()
-            print "after reducing radius is " + str(len(json_response['locations']))
-            markdown_reply = format_json_response(json_response, message["location"], search_parameters['radius'], original_merchant_length)
+            # print "after reducing radius is " + str(len(json_response['locations']))
+            # markdown_reply = format_json_response(json_response, message["location"], search_parameters['radius'], original_merchant_length)
+            markdown_reply = format_json_response(json_response)
             # print markdown_reply
             print "size of markdown reply" + str(sys.getsizeof(markdown_reply))
 
-            reply_markup={"inline_keyboard":[[{"text": "1", "callback_data": "test1"}]]}
+            reply_markup={"inline_keyboard":[[{"text": "Page 2", "callback_data": 2}]]}
 
 
             # data = {"text": markdown_reply.encode("utf8"), "chat_id": chat_id, "parse_mode": "markdown", "disable_web_page_preview": True}
